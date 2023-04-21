2a3d68860ad45f4d2a9e28b9a6ee6a2a49451ca9
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-21 20:36:47 +0400

    configured typeormModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,28 @@
-import { ApolloDriver } from '@nestjs/apollo';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
+import { LessonResolver } from './lesson/lesson.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
+import { LessonService } from './lesson/lesson.service';
+import { join } from 'path';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      auteSchemaFile: true,
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
       driver: ApolloDriver,
     }),
     LessonModule,
   ],
+  providers: [LessonResolver],
 })
 export class AppModule {}
