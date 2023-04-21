9a7b789c5cf13b077725e44b69ab514960ab4618
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-21 20:55:38 +0400

    modified dto for graphql resolver

--- src/lesson/dto/create-lesson.dto.ts
+++ src/lesson/dto/create-lesson.dto.ts
@@ -1,12 +1,18 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, MinLength } from 'class-validator';
 
+@InputType()
 export class CreateLessonDto {
   @IsNotEmpty()
+  @MinLength(1)
+  @Field()
   name: string;
 
+  @Field()
   @IsOptional()
   startDate: string;
 
+  @Field()
   @IsOptional()
   endDate: string;
 }
