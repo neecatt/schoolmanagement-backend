446951f98225b0fa5897f36fa7730fe5eaa52473
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-23 02:10:35 +0400

    added student entity

---
+++ src/student/student.entity.ts
@@ -0,0 +1,17 @@
+import { Field } from '@nestjs/graphql';
+import { Column, Entity, ObjectIdColumn, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Student {
+  @ObjectIdColumn()
+  _id: string;
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+}
