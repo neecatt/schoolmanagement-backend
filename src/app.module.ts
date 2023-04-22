6557546faba92b1817ffc69c358b25ceb5c8d6b8
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-23 02:11:50 +0400

    added student entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson/lesson.entity';
 import { LessonService } from './lesson/lesson.service';
 import { join } from 'path';
+import { StudentModule } from './student/student.module';
+import { dir } from 'console';
+import { Student } from './student/student.entity';
 
 @Module({
   imports: [
@@ -15,13 +18,14 @@ import { join } from 'path';
       url: 'mongodb://localhost/school',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Lesson],
+      entities: [Lesson, Student],
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver,
     }),
     LessonModule,
+    StudentModule,
   ],
   providers: [LessonResolver],
 })
